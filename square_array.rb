dd8d79529fbb18e43dc7101659682dc02a17b870
Donovan Jones <donovanljones94@gmail.com>
2019-08-18 00:17:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,6 @@ def square_array(array)
     new_array = []
     square = number * number
     new_array.push (square)
-    if new_array.length == array.length
-      return new_array
-    end
   end
+  return new_array
 end
