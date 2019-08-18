c73dd9315f286922bbcdc4d4de5e31aa7963fc4d
Donovan Jones <donovanljones94@gmail.com>
2019-08-18 00:11:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,6 @@ def square_array(array)
   array.each do |number|
     new_array = []
     square = number * number
-    new_array.push (number)
-    new_array
+    new_array.push (square)
   end
 end
